e5ea5fc0fef58e6b47c546767852b6a766fad24a
Chunxiang Zheng <ranger083@gmail.com>
2014-10-18 17:13:21 -0700

    Create RatioDB.java

---
+++ RatioDB.java
@@ -0,0 +1,13 @@
+/*
+ * store the protein name, ratio for condition 1 and ratio for condition 2
+ */
+public class RatioDB {
+	String proName;
+	double ratioML;
+	double stdevML;
+	double ratioUL;
+	double stdevUL;
+	public RatioDB() {
+		
+	}
+}
